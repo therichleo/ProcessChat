8b804e86877c812828ccb9ad2e39515dba7ca4ee
therichleo <leonardo.valdes@mail.udp.cl>
2025-09-03 14:48:54 -0400

    Updates

--- Cliente1/cliente.c
+++ Cliente1/cliente.c
@@ -10,24 +10,26 @@ Y DAR UN MENU DE OPCIONES DE REPORTES O DE MENSAJE
 #include <sys/types.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+
 
 int main(){
 
     int fd;
 
-    const char *fifo_path = "Users/leonardovaldeslizana/Proyects/ProcessChat/Server";
+    const char *fifo_path = "Users/leonardovaldeslizana/Proyects/ProcessChat/Cliente1";
     mode_t fifo_permissions = 0666;
 
-
-    pid_t my_pid = getpid();
-    printf("My PID: %d\n", my_pid);
-
-
     mkfifo(fifo_path,fifo_permissions);
-
     fd = open(fifo_path,O_WRONLY);
+    pid_t my_pid = getpid();
 
     write(fd, &my_pid, sizeof(my_pid));
+    printf("My PID: %d\n", my_pid);
+
     
 
+    close(fd);
+    unlink(fifo_path);
+    return 0;
 }
--- Server/server.c
+++ Server/server.c
@@ -1,5 +1,5 @@
 #include <sys/stat.h> //Para fifo y mode t
-#include <fcntl.h>. //PARA O_WRONLY (habilita que el archivo pipe pueda escribir pero no leer)
+#include <fcntl.h> //PARA O_WRONLY (habilita que el archivo pipe pueda escribir pero no leer)
 #include <unistd.h> //Para open() and close()
 
 /*
@@ -11,9 +11,20 @@ y si alguien quiere charlar con otro nodo, el nodo enviara el mensaje y dira a q
 int main(){
 
     int fd;
+    pid_t my_pid;
 
     const char *fifo_path = "Users/leonardovaldeslizana/Proyects/ProcessChat/Cliente1";
     mode_t fifo_permissions = 0666; // permisos para lectura y escritura
 
     mkfifo(fifo_path,fifo_permissions);
+    
+    fd = open(fifo_path, fifo_permissions);
+
+    while(1){
+        read(fd, &my_pid, sizeof(my_pid));
+
+    }
+    close(fd);
+    unlink(fifo_path);
+    return 0;
 }
