9b0ad64248d7163cb0a20427a9ba12650c5e7abf
therichleo <leonardo.valdes@mail.udp.cl>
2025-09-03 09:25:46 -0400

    Initial commit

--- Server/server.c
+++ Server/server.c
@@ -2,6 +2,12 @@
 #include <fcntl.h>. //PARA O_WRONLY (habilita que el archivo pipe pueda escribir pero no leer)
 #include <unistd.h> //Para open() and close()
 
+/*
+ICMP Echo Requests
+utilizare este protocolo como seguimiento
+al iniciar un cliente enviara su PID al server, este la recibe y les dice a todos, estos son los nodos disponibles para charlar
+y si alguien quiere charlar con otro nodo, el nodo enviara el mensaje y dira a que pid quiere llegar, el server le mandara el mensaje hacia alla
+*/
 int main(){
 
     int fd;
