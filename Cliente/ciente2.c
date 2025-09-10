c2108df4b4a4ddb68074d86ab682efd6458dafd9
therichleo <leonardo.valdes@mail.udp.cl>
2025-09-09 21:50:34 -0300

    update

--- Cliente/ciente2.c
+++ Cliente/ciente2.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define MSG_SIZE 256
 typedef struct {
@@ -13,17 +14,12 @@ typedef struct {
     char mensaje[MSG_SIZE];
 } estructure;
 
-const char *ChatGeneralFIFO = "tmp/processchat_comunnity_chat"; //PIPE donde el server enviara mensajes generales
-const char *ClientTalkFIFO = "tmp/processchat_client_talk"; //PIPE donde clientes envian mensaje al server
-int fd;
-int fd2;
+const char *ClientTalkFIFO = "/tmp/processchat_client_talk"; //PIPE donde clientes envian mensaje al server
+int fd2 = -1;
 
 //FUNCION PARA CTRL+C
 void cleanup_handler(int sig){
     printf("\nCerrando cliente...\n");
-    if (fd != -1) {
-        close(fd);
-    }
     if(fd2 != -1){
         close(fd2);
     }
@@ -35,30 +31,32 @@ int main(){
     signal(SIGINT, cleanup_handler); //interrupciones como control + c
 
     pid_t mypid = getpid(); //conseguimos nuestra PID
-    printf("Hola mi PID es: %d",&mypid); 
-
-    fd = open(ChatGeneralFIFO, O_RDONLY); //Lee mensajes del servidor
-    if(fd == -1){
-        printf("Error abriendo PIPE ChatGeneralFIFO en Cliente de pid: %d", &mypid);
-    }
+    printf("Hola mi PID es: %d \n",(int)mypid); 
     
     fd2 = open(ClientTalkFIFO,O_WRONLY); //escribe mensajes hacia el servidor
     if(fd2 == -1){
-        printf("Error arbiendo PIPE ClientTalkFIFO en Cliente de pid: $d", &mypid);
+        printf("Error arbiendo PIPE ClientTalkFIFO en Cliente de pid: %d \n",(int)mypid);
     }
 
 
     estructure pkt;
-    ssize_t n;
-    char temp[1200];
-
-    while(1){}    
-
+    printf("Escribe mensaje:");
+    ssize_t bytes_read;
+    char msg[256];
 
 
-
-
-
+    while(1){
+        
+        fgets(pkt.mensaje, MSG_SIZE, stdin); //lee de teclado y lo guarda en variable pkt.mensaje y el tamaño de bytes de msg_size
+        size_t L = strlen(pkt.mensaje);  //calcula longitud de la cadena leida
+        if (L && pkt.mensaje[L-1] == '\n') pkt.mensaje[L-1] = '\0'; //limpia mensaje, quita el salto de linea por el 0
+        if (strcmp(pkt.mensaje, "-1") == 0) { //compara con el -1
+            printf("Cliente finalizado por el usuario.\n");
+            cleanup_handler(0);
+        }
+        pkt.pid = mypid;
+        write(fd2,&pkt, sizeof(pkt));
+    }    
 
 
 }
--- Server/server2.c
+++ Server/server2.c
@@ -1,116 +1,75 @@
-#include <sys/stat.h> //Para fifo y mode t
-#include <fcntl.h> //PARA O_WRONLY (habilita que el archivo pipe pueda escribir pero no leer)
-#include <unistd.h> //Para open() and close()
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
 #include <stdlib.h>
 
-//estructura para envio y recibo de mensajes, esto hace que envien PID con mensaje
 #define MSG_SIZE 256
 typedef struct {
     int pid;
     char mensaje[MSG_SIZE];
 } estructure;
 
-const char *ChatGeneralFIFO = "tmp/processchat_comunnity_chat"; //PIPE donde el server enviara mensajes generales
-const char *ClientTalkFIFO = "tmp/processchat_client_talk"; //PIPE donde clientes envian mensaje al server
-int fd;
-int fd2;
-
+const char *ClientTalkFIFO = "/tmp/processchat_client_talk"; // clientes -> servidor
+int fd2 = -1;
 
 void cleanup_handler(int sig){
+    (void)sig;
     printf("\nCerrando servidor...\n");
-    if(fd != -1) {
-        close(fd); //Si no hubo error el cual cerrara la pipe, signfica que la pipe sigue, entonces la cierra
-    }
-    if(fd2 != -1){
-        close(fd2); //Si no hubo error el cual cerrara la pipe, signfica que la pipe sigue, entonces la cierra
-    }
-    unlink(ChatGeneralFIFO);
+    if (fd2 != -1) close(fd2); //cierra PIPE si esque se hizo un control c y sigue activa
     unlink(ClientTalkFIFO);
     exit(0);
 }
 
-int main(){
-
-    //Configurar manejador de señales para limpieza para CTRL+C
+int main(void){
     signal(SIGINT, cleanup_handler);
 
-    //limpia los pipes
-    unlink(ChatGeneralFIFO);
-    unlink(ClientTalkFIFO);
+    unlink(ClientTalkFIFO); //limpia la PIPE
 
-    int x;
-    ssize_t bytes_read;
     mode_t fifo_permissions = 0666; //permisos para lectura y escritura
-
-    //Crear FIFO server talks, en donde el central enviara los mensajes de los logs
-    if(mkfifo(ChatGeneralFIFO, fifo_permissions) == -1 ){
-        
-        printf("Error creando FIFO ChatGeneralFIFO");
-        return(1);
-        
+    if (mkfifo(ClientTalkFIFO, fifo_permissions) == -1) {
+        fprintf(stderr, "Error creando FIFO %s: %s\n", ClientTalkFIFO, strerror(errno));
+        return 1;
     }
-    printf("ChatGeneralFIFO creado exitosamente... \n");
+    printf("FIFO creada: %s\n", ClientTalkFIFO);
 
-    //Crear FIFO Client talks, en donde el cliente enviara mensajes, el server los escucha
-    if(mkfifo(ClientTalkFIFO, fifo_permissions) == -1 ){
-        printf("Error creando FIFO ClientTalkFIFO");
-        return(1);
-        
-    }
-    printf("ClientTalkFIFO creado exitosamente... \n");
-
-    /*
-    IDEA:
-    El cliente envia mensajes por ClientTalksFIFO, el server los escucha
-    El Server reenvia los mensajes por ChatGeneralFIFO, y los clientes lo escuchan
-    */
-
-    //Abrimos pipe en modo Lectura
-    fd = open(ChatGeneralFIFO, O_WRONLY);
-    if(fd == -1){
-        printf("Error abriendo la PIPE ChatGeneralFIFO");
-        return(1);
-    }
-
-    //Abrimos pipe en modo escritura
+    // Abre para lectura (bloquea hasta que un cliente abra en escritura)
     fd2 = open(ClientTalkFIFO, O_RDONLY);
-    if(fd2 == -1){
-        printf("Error abriendo la PIPE ClientTalkFIFO");
+    if (fd2 == -1){
+        fprintf(stderr, "Error abriendo FIFO %s en lectura: %s\n", ClientTalkFIFO, strerror(errno));
+        cleanup_handler(0);
     }
 
+    printf("Servidor listo. Esperando mensajes...\n");
 
-    int chats;
-    printf("Cantidad de chats a crear: ");
-    scanf("%d",&chats);
-
-    while(chats){ 
-
-
-        //crear el numero de chats, los cuales cada chat sea acompañado con su chat
-
-
-
-        --chats;
-    }
-    
     estructure pkt;
     ssize_t n;
-    char temp[1200];
 
-    while(1){
-        while ((n = read(fd2, &pkt, sizeof(pkt))) > 0) { //Si lee algo entra
-        if (n != sizeof(pkt)) {
-            continue;
-        }
-        printf("PID=%d, Mensaje=\"%s\"\n", pkt.pid, pkt.mensaje);
-        snprintf(temp, sizeof(temp), "%d: %s", pkt.pid, pkt.mensaje);  //Pasa los datos PID y Mensaje a un char "PID: Mensaje"
-        write(fd, temp, strlen(temp) + 1); //Se pone +1 ya que en una cadena de char en C es 'H' 'O' 'L' 'A' '\0'
+    while (1) {
+        n = read(fd2, &pkt, sizeof(pkt));
+        if (n > 0) {
+            if (n != sizeof(pkt)) {
+                continue;
+            }
+            pkt.mensaje[MSG_SIZE - 1] = '\0';
+            printf("PID=%d, Mensaje=\"%s\"\n", pkt.pid, pkt.mensaje);
+        } else if (n == 0) { //si nadie escribe, la cierra y la reabre para nuevas personas entrando
+            printf("No hay clientes escribiendo. Esperando nuevos...\n");
+            close(fd2);
+            fd2 = open(ClientTalkFIFO, O_RDONLY);
+            if (fd2 == -1){
+                fprintf(stderr, "Error reabriendo FIFO %s: %s\n", ClientTalkFIFO, strerror(errno));
+                cleanup_handler(0);
+            }
+        } else {
+            perror("read");
+            break;
         }
     }
-    
-    return(0);
+
+    cleanup_handler(0);
+    return 0;
 }
